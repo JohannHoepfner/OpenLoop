6967509eb0777f3c15ba9800d9f888ca51a2874e
Johann Höpfner <johannhoepfner@protonmail.com>
2022-03-22 13:13:57 +0100

    precompile script for 100x performance boost

--- OpenLoopRun/Runner.cs
+++ OpenLoopRun/Runner.cs
@@ -5,7 +5,7 @@ namespace OpenLoopRun
 	public class Runner
 	{
 		public OpenLoopProgram Program { get; set; }
-
+		List<(string Variable, IGenericExpression<double> Term)> LoopCodeCompiled = new();
 		readonly ExpressionContext context;
 
 		public List<Dictionary<string, double>> VarHistory { get; private set; }
@@ -35,29 +35,51 @@ namespace OpenLoopRun
 			VarHistory = new();
 			Program = new();
 		}
-
-		public void RunLine(string line)
-		{
-			var leftSide = line[..line.IndexOf('=')];
-			var rightSide = line[(line.IndexOf('=') + 1)..];
-			IGenericExpression<double> rightTerm = context.CompileGeneric<double>(rightSide);
-			context.Variables[leftSide] = rightTerm.Evaluate();
-		}
 		public void Start()
 		{
 			foreach (var line in Program.StartCode)
 			{
-				RunLine(line);
+				var leftSide = (line[..line.IndexOf('=')]).Trim();
+				var rightSide = (line[(line.IndexOf('=') + 1)..]).Trim();
+				IGenericExpression<double> rightTerm = context.CompileGeneric<double>(rightSide);
+				context.Variables[leftSide] = rightTerm.Evaluate();
+			}
+			var dict = new Dictionary<string, double>();
+			foreach (var key in context.Variables.Keys)
+			{
+				dict.Add(key, (double)context.Variables[key]);
+			}
+			VarHistory.Add(dict);
+			foreach (var line in Program.LoopCode)
+			{
+				var leftSide = (line[..line.IndexOf('=')]).Trim();
+				var rightSide = (line[(line.IndexOf('=') + 1)..]).Trim();
+				IGenericExpression<double> rightTerm = context.CompileGeneric<double>(rightSide);
+				context.Variables[leftSide] = rightTerm.Evaluate();
+			}
+			dict = new Dictionary<string, double>();
+			foreach (var key in context.Variables.Keys)
+			{
+				dict.Add(key, (double)context.Variables[key]);
+			}
+			VarHistory.Add(dict);
+			foreach (var line in Program.LoopCode)
+			{
+				var leftSide = (line[..line.IndexOf('=')]).Trim();
+				var rightSide = (line[(line.IndexOf('=') + 1)..]).Trim();
+				LoopCodeCompiled.Add((
+					leftSide,
+					context.CompileGeneric<double>(rightSide)
+					));
 			}
 		}
 		public void Step()
 		{
-			foreach (var line in Program.LoopCode)
+			foreach (var line in LoopCodeCompiled)
 			{
-				RunLine(line);
+				context.Variables[line.Variable] = line.Term.Evaluate();
 			}
 			var dict = new Dictionary<string, double>();
-
 			foreach (var key in context.Variables.Keys)
 			{
 				dict.Add(key, (double)context.Variables[key]);
